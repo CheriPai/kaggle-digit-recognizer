14e8accd7036edc8bf750d5336899853c3f255b0
cheripai <datdo@rocketmail.com>
2016-02-21 17:30:35 -0800

    Add more hidden layers

--- train.py
+++ train.py
@@ -3,7 +3,6 @@ from sklearn import cross_validation as cv
 from sklearn.externals import joblib
 from sknn.mlp import Classifier, Convolution, Layer
 
-
 df = pd.read_csv('data/train.csv')
 
 X = df.drop('label', axis=1).values
@@ -14,9 +13,14 @@ X_train, X_test, y_train, y_test = cv.train_test_split(X, y, test_size=0.1, rand
 
 # Create neural network
 clf = Classifier(
-    layers=[Convolution('Rectifier', channels=8, kernel_shape=(3, 3)), Layer('Softmax')],
+    layers=[
+        Convolution('Rectifier', channels=20, kernel_shape=(5, 5), pool_shape=(2, 2)),
+        Layer('Rectifier', units=20*12*12),
+        Layer('Softmax')
+    ],
     learning_rate=0.00002,
-    n_iter=10
+    n_iter=10,
+    verbose=True
 )
 clf.fit(X_train, y_train)
 
