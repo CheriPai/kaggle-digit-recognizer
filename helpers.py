ade05cda53e88091cfd4a8d479125ac4a9ac72b4
cheripai <datdo@rocketmail.com>
2016-03-15 23:08:17 -0700

    Use convolutional neural network

--- helpers.py
+++ helpers.py
@@ -1,4 +1,6 @@
 import numpy as np
+import tensorflow as tf
+
 
 epochs_completed = 0
 index_in_epoch = 0
@@ -34,3 +36,21 @@ def next_batch(images, labels, batch_size):
         assert batch_size <= num_examples
     end = index_in_epoch
     return images[start:end], labels[start:end]
+
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-import helpers
+import helpers as h
 import numpy as np
 import pandas as pd
 import tensorflow as tf
@@ -7,16 +7,16 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 # Configuration
 VALIDATION_SIZE = 2000
-LEARNING_RATE = 0.01
-TRAINING_ITERATIONS = 1000
-BATCH_SIZE = 100
+LEARNING_RATE = 1e-4
+TRAINING_ITERATIONS = 20000
+BATCH_SIZE = 50
 
 
 # Set up data and convert to one hot
 data = pd.read_csv('data/train.csv')
 images = data.iloc[:,1:].values.astype(np.float)
 images = np.multiply(images, 1.0 / 255.0)
-labels = helpers.dense_to_one_hot(data[[0]].values.ravel(), 10).astype(np.uint8)
+labels = h.dense_to_one_hot(data[[0]].values.ravel(), 10).astype(np.uint8)
 
 
 # Split data into cross validation and training set
@@ -28,30 +28,60 @@ train_labels = labels[VALIDATION_SIZE:]
 
 # Initialize network variables
 x = tf.placeholder(tf.float32, [None, 784])
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
+x_image = tf.reshape(x, [-1, 28, 28, 1])
 
-y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+# Create network layers
+W_conv1 = h.weight_variable([5, 5, 1, 32])
+b_conv1 = h.bias_variable([32])
+h_conv1 = tf.nn.relu(h.conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = h.max_pool_2x2(h_conv1)
+
+
+W_conv2 = h.weight_variable([5, 5, 32, 64])
+b_conv2 = h.bias_variable([64])
+h_conv2 = tf.nn.relu(h.conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = h.max_pool_2x2(h_conv2)
+
+
+W_fc1 = h.weight_variable([7*7*64, 1024])
+b_fc1 = h.bias_variable([1024])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+
+# Apply dropout to reduce overfitting
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+
+# Create readout layer
+W_fc2 = h.weight_variable([1024, 10])
+b_fc2 = h.bias_variable([10])
+y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 y_ = tf.placeholder(tf.float32, [None, 10])
 
 
 # Use cross entropy cost function
-cross_entropy = -tf.reduce_sum(y_*tf.log(y))
-train_step = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(cross_entropy)
+cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
+train_step = tf.train.AdamOptimizer(LEARNING_RATE).minimize(cross_entropy)
 
 
-init = tf.initialize_all_variables()
 sess = tf.Session()
-sess.run(init)
+sess.run(tf.initialize_all_variables())
+saver = tf.train.Saver()
 
 
 # Train using stochastic gradient descent
-for i in range(TRAINING_ITERATIONS):
-    batch_xs, batch_ys = helpers.next_batch(train_images, train_labels, BATCH_SIZE)
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-
-
-# Calcluate and print accuracy
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print('Accuracy: {}'.format(sess.run(accuracy, feed_dict={x: cv_images, y_: cv_labels})))
+for i in range(TRAINING_ITERATIONS):
+    batch_xs, batch_ys = h.next_batch(train_images, train_labels, BATCH_SIZE)
+    if i % 100 == 0:
+        train_accuracy = sess.run(accuracy, feed_dict={x:batch_xs, y_: batch_ys, keep_prob: 1.0})
+        print("step {}, training accuracy {}".format(i, train_accuracy))
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 0.5})
+
+
+print('Accuracy: {}'.format(sess.run(accuracy, feed_dict={x: cv_images, y_: cv_labels, keep_prob: 1.0})))
+saver.save(sess, "data/output/model.ckpt")
