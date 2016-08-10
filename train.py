a3334e2039f167452db3cf214e297a2a6eb6cb3b
cheripai <datdo@rocketmail.com>
2016-08-09 19:08:04 -0700

    Change early stop to watch val_loss

--- train.py
+++ train.py
@@ -54,7 +54,7 @@ def train_model(pretrain):
         model = build_model()
 
     model.compile(loss="categorical_crossentropy", optimizer="adadelta", metrics=["accuracy"])
-    early_stop = EarlyStopping(monitor="val_acc", patience=8, mode="max")
+    early_stop = EarlyStopping(monitor="val_loss", patience=8, mode="min")
     model.fit(X, y, batch_size=BATCH_SIZE, nb_epoch=100, 
         validation_data=(X_val, y_val), callbacks=[early_stop])
 
